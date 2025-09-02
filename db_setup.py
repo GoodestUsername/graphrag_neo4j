0a0fedb57a6dde180d429b014bd696e5ddfd6e89
Goodestusername <ericdongcannn@hotmail.com>
2025-09-02 02:44:21 -0700

    added graph vector index

--- db_setup.py
+++ db_setup.py
@@ -15,3 +15,13 @@ def create_text_index(driver: Driver):
     driver.execute_query(
         "CREATE FULLTEXT INDEX PdfChunkFullText FOR (c:Chunk) ON EACH [c.text]"
     )
+
+
+def create_graph_vector_index(driver: Driver):
+    driver.execute_query(
+        """
+            CREATE VECTOR INDEX parent IF NOT EXISTS
+            FOR (c:Child)
+            ON c.embedding
+        """
+    )
